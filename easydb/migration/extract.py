acc232c854d35da79fb189866231ee28e96a5053
to7be <tobias.waitschies@gmail.com>
2016-12-06 08:50:28 +0100

    Added file header containing copyright information
    Affected file extensions:
    *.py

--- easydb/migration/extract.py
+++ easydb/migration/extract.py
@@ -1,3 +1,10 @@
+'''
+* easydb-migration-tools - easydb Migration Tools
+ * Copyright (c) 2013 - 2016 Programmfabrik GmbH
+ * MIT Licence
+ * https://github.com/programmfabrik/coffeescript-ui, http://www.coffeescript-ui.org
+'''
+
 import sqlite3
 import psycopg2
 import psycopg2.extensions
---
+++ easydb/server/__init__.py
@@ -0,0 +1,6 @@
+'''
+* easydb-migration-tools - easydb Migration Tools
+ * Copyright (c) 2013 - 2016 Programmfabrik GmbH
+ * MIT Licence
+ * https://github.com/programmfabrik/coffeescript-ui, http://www.coffeescript-ui.org
+'''
