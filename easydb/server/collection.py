a5dd30cbdf24a5bc7beb6672d6cf99fd04d1189d
Oliver Flum <oliver@programmfabrik.de>
2017-03-29 12:29:10 +0200

    Added missing files

---
+++ easydb/server/collection.py
@@ -0,0 +1,82 @@
+'''
+* easydb-migration-tools - easydb Migration Tools
+ * Copyright (c) 2013 - 2016 Programmfabrik GmbH
+ * MIT Licence
+ * https://github.com/programmfabrik/coffeescript-ui, http://www.coffeescript-ui.org
+'''
+
+import json
+
+class Collection(object):
+
+    def __init__(self):
+        self.displayname = {}
+        self.description= {}
+        self.parent_id = None
+        self.id = None
+        self.version = 1
+        self.source_id = None
+        self.owner = None
+        self.owner_id=None
+        self.user_collection_id=None
+
+    def to_json(self):
+        if self.user_collection_id is not None:
+            js = {
+                'collection': {
+                    '_id': self.id,
+                    '_version': self.version,
+                    'displayname': self.displayname,
+                    'description': self.description,
+                    '_id_parent': self.user_collection_id,
+                    'children_allowed': True
+                    },
+                    '_owner': {
+                    '_basetype': 'user',
+                    'user':
+                        {
+                        '_id': self.owner_id
+                        }
+                    }
+                }
+        if self.user_collection_id is None:
+            js = {
+                'collection': {
+                    '_id': self.id,
+                    '_version': self.version,
+                    'displayname': self.displayname,
+                    'description': self.description,
+                    '_id_parent': self.parent_id,
+                    'children_allowed': True
+                    },
+                    '_owner': {
+                    '_basetype': 'user',
+                    'user':
+                        {
+                        '_id': self.owner_id
+                        }
+                    }
+                }
+        return js
+
+    @staticmethod
+    def from_row(row):
+        collection = Collection()
+
+        for key, value in row.items():
+
+            if key.startswith('displayname:'):
+                collection.displayname[key.split(':')[1]] = value
+            if key.startswith('description:'):
+                collection.description[key.split(':')[1]] = value
+            if key == '__parent_id':
+                collection.parent_id = value
+            if key == '__source_unique_id':
+                collection.source_id = value
+            if key == '__owner':
+                collection.owner = value
+            if key == '__owner_id':
+                collection.owner_id=value
+            if key == '__user_collection_id':
+                collection.user_collection_id=value
+        return collection
---
+++ easydb/server/collection_object.py
@@ -0,0 +1,45 @@
+'''
+* easydb-migration-tools - easydb Migration Tools
+ * Copyright (c) 2013 - 2016 Programmfabrik GmbH
+ * MIT Licence
+ * https://github.com/programmfabrik/coffeescript-ui, http://www.coffeescript-ui.org
+'''
+
+import json
+
+class Collection_Object(object):
+
+    def __init__(self):
+        self.collection_id=None
+        self.object_id=None
+        self.id = None
+        self.version = 1
+        self.source_id = None
+        self.uploaded=None
+        self.object_goid=None
+
+    def to_json(self):
+        js = {'objects':[
+                {
+                "_global_object_id": str(self.object_goid)
+                }
+            ]}
+        print(js)
+        return js
+
+    @staticmethod
+    def from_row(row):
+        collection_object = Collection_Object()
+
+        for key, value in row.items():
+            if key == '__source_unique_id':
+                collection_object.source_id = value
+            if key == 'collection_id':
+                collection_object.collection_id=value
+            if key == 'object_id':
+                collection_object.object_id=value
+            if key == 'uploaded':
+                collection_object.uploaded=value
+            if key == 'object_goid':
+                collection_object.object_goid=value
+        return collection_object
