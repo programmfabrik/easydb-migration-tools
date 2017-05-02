86ef7dbe1ee9e31bb515b3b13073dd3a2e333d85
Oliver Flum <oliver@programmfabrik.de>
2017-05-02 14:27:40 +0200

    testing

--- easydb/server/api.py
+++ easydb/server/api.py
@@ -169,7 +169,9 @@ class EasydbAPI(object):
         if len(collection_objects) == 0:
             return
         for collection_object in collection_objects:
-            call="collection/objects/push/{}".format(collection_object.collection_id)
+            if collection_object.object_goid=None:
+                continue
+            call="collection/objects/{}".format(collection_object.collection_id)
             self.logger.info("POST {}".format(call))
             response_object = self.post(call, collection_object.to_json())
             self.logger.debug('RESPONSE COLLECTION UPDATE:\n {0}'.format(response_object))
