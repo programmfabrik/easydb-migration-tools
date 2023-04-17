9bdd1ebf21547cf109cf5a0bafedaeba835013b2
Philipp Hempel <philipp.hempel@programmfabrik.de>
2023-04-17 13:19:19 +0200

    finished simplifed data2sqlite version that only supports the mode file --K10plus; see #65693

--- data2sqlite.py
+++ data2sqlite.py
@@ -3,70 +3,50 @@
 
 import sys
 import argparse
-import os
 import easydb.migration.extract as extract
 import logging
 import logging.config
-import requests
 
-logging.basicConfig(level=logging.WARN)
+logging.basicConfig(level=logging.INFO)
 
-argparser = argparse.ArgumentParser(description='data2qlite')
+argparser = argparse.ArgumentParser(description='!!! simplified version: only file -K10plus !!! | data2qlite')
 
-argparser.add_argument('-t', '--target', default='dump.db',
-                       help='Sqlite-Dump name and directory (Default: ./dump.db)')
-argparser.add_argument('--init', action='store_true',
-                       help='If set, existing files will be purged')
-argparser.add_argument('--name', default='source',
-                       help='NAME in target db (required, default: "source")')
-argparser.add_argument('-s', '--silent', action='store_true',
-                       help='If set, don\'t output progress every 100 rows.')
+argparser.add_argument('-t', '--target', default='dump.db', help='Sqlite-Dump name and directory (Default: ./dump.db)')
+argparser.add_argument('--init', action='store_true', help='If set, existing files will be purged')
+argparser.add_argument('--name', default='source', help='NAME in target db (required, default: "source")')
+argparser.add_argument('-s', '--silent', action='store_true', help='If set, don\'t output progress every 100 rows.')
 
 subparsers = argparser.add_subparsers(dest='mode', help='Set Datasources')
 
-import_parser = subparsers.add_parser(
-    'file', help='Add to Source from other files')
-import_parser.add_argument('--K10plus', nargs='*', default=[],
-                           help='Filename(s) for k10plus (PICA+ format, .pp files)')
+import_parser = subparsers.add_parser('file', help='Add to Source from other files')
+import_parser.add_argument('--K10plus', nargs='*', default=[], help='Filename(s) for k10plus (PICA+ format, .pp files)')
 
 global args
 
 args = argparser.parse_args()
 
-if args.mode not in [
-    'easydb4',
-    'pg',
-    'mysql',
-    'file',
-    'adhh',
-]:
-    argparser.print_usage(sys.stdout)
-    sys.exit()
-
-extract.__sqlite_init()
+extract.sqlite_init()
 extract.prepare_source(args.target, init=args.init)
 
 extract.args = args
-
-
-# FILE-IMPORT
 
 
 if args.mode == 'file':
 
     source_file = args.target
 
-
     if args.K10plus != []:
         for k10plus_file in args.K10plus:
             item_id_offset, offset = extract.k10plus_get_offset(k10plus_file)
-            logging.info('Adding "' + k10plus_file +
-                         '" to Source (start offset:' + str(offset) + ')')
+            logging.info('Adding "' + k10plus_file + '" to Source (start offset:' + str(offset) + ')')
             item_id_offset = extract.k10plus_to_source(
                 filename=k10plus_file,
                 item_id_offset=item_id_offset,
                 offset=offset)
 
+else:
+    argparser.print_usage(sys.stdout)
+    sys.exit()
 
 
-extract.__commit_source()
+extract.commit_source()
--- easydb/migration/extract.py
+++ easydb/migration/extract.py
@@ -10,7 +10,6 @@ import os
 import sys
 import decimal
 import json
-import requests
 import chardet
 import datetime
 
@@ -27,7 +26,7 @@ except NameError:
     silent = False
 
 
-def __sqlite_init():
+def sqlite_init():
     def adapt_list(l):
         return json.dumps(l)
 
@@ -88,7 +87,7 @@ def __execute(cursor, sql, bindings=None):
         raise e
 
 
-def __commit_source():
+def commit_source():
     global source_conn
     if source_conn:
         source_conn.commit()
@@ -96,291 +95,7 @@ def __commit_source():
         print("Notice: Commited source.")
 
 
-def res_get_json(res):
-    if not hasattr(res, "json"):
-        # very old python request, no json attribute
-        return json.loads(res.content)
-    elif isinstance(res.json, dict):
-        # old python.requests module: json is an object
-        return res.json
-    else:
-        # new python.requests module: json is a method
-        return res.json()
-
-
-def __store_eas_id(
-    name,              # name of source
-    url,               # url of eas
-    instance,          # instance of eas
-    eas_id,            # id for asset
-    table_name,        # table name in source
-    source_unique_id,  # id in source
-    column_name,       # column in source
-    eas_versions={"original": ["url"]}
-):
-
-    if isinstance(eas_id, list):
-        _eas_ids_a = list(map(str, eas_id))
-        assert(isinstance(source_unique_id, list))
-        _source_unique_ids_a = source_unique_id
-    else:
-        _eas_ids_a = [str(eas_id), ]
-        _source_unique_ids_a = [source_unique_id, ]
-
-    req = url + "/bulkversions?instance=" + instance + \
-        "&asset_ids=[" + ",".join(_eas_ids_a) + "]"
-
-    _res = requests.get(req)
-
-    if _res.status_code != 200:
-        print("""Warning: EAS-ID %s not found or error from EAS-Server. Status: "%s".""" %
-              (_eas_ids_a, _res.status_code), _res.text)
-        return False
-
-    res = res_get_json(_res)
-
-    count = 0
-    skips = 0
-
-    for i in range(len(_eas_ids_a)):
-        _eas_id = _eas_ids_a[i]
-        _source_unique_id = _source_unique_ids_a[i]
-
-        for (eas_version, store_as) in list(eas_versions.items()):
-            count += 1
-            original = None
-
-            for version in res[_eas_id]["versions"]:
-                if version["version"] == "original":
-                    original = version
-                    break
-
-            if not original:
-                print("Warning: EAS-ID", _eas_id, "Original not found.""")
-                return False
-
-            use_version = None
-            for version in res[_eas_id]["versions"]:
-                if version["version"] == eas_version:
-                    use_version = version
-                    break
-
-            if not use_version:
-                print("Notice: EAS-ID", _eas_id, "Version",
-                      eas_version, """not found.""")
-                # skip
-                skips += 1
-                continue
-
-            if use_version["status"] != "done":
-                print("""Warning: EAS-ID %s, Version "%s" has status "%s", Request: "%s", skipping.""" %
-                      (_eas_id, use_version["version"], use_version["status"], req))
-                # skip
-                skips += 1
-                continue
-
-            eas_root_id = res[_eas_id]["root_id"]
-
-            if eas_root_id:
-                # we need to insert this first, because of a foreign key we
-                # have
-                ret = __store_eas_id(
-                    name=name,
-                    instance=instance,
-                    url=url,
-                    eas_id=eas_root_id,
-                    eas_versions={eas_version: store_as},
-                    table_name=table_name,
-                    source_unique_id=_source_unique_id,
-                    column_name=column_name
-                )
-                if ret == False:
-                    print(
-                        "Warning: Could not insert root id, not storing root_id", eas_root_id)
-                    eas_root_id = None
-
-            __store_file_from_url(
-                url=url + use_version["link"],
-                name=name,
-                source_table_name=table_name,
-                source_unique_id=_source_unique_id,
-                source_column_name=column_name,
-                original_filename=original["custom"]["original_filename"],
-                file_unique_id=use_version["hash"],
-                eas_id=int(_eas_id),
-                eas_root_id=eas_root_id,
-                file_version=eas_version,
-                store_as=store_as
-            )
-
-    if skips > 0:
-        print("Warning: %s out of %s versions failed to export." % (skips, count))
-        return False
-    else:
-        return True
-
-# stores file as blob from url
-
-
-def __store_file_from_url(
-    name,                    # name of source
-    url,                     # url to fetch data from
-    source_table_name,       # table this is meant for
-    source_unique_id,        # row in table this is meant for
-    source_column_name,      # column in table this is meant for
-    file_unique_id,          # a unique id provided to store the file
-    eas_id=None,             # optional eas-id
-    eas_root_id=None,        # optional eas-root-id
-    file_version=None,       # optional version
-    original_filename=None,  # original_filename of file
-    store_as=["data"]        # allowed values are "data", "url", "file"
-):
-
-    global silent
-
-    if eas_id:
-        info = "EAS-ID:" + str(eas_id) + " [" + file_version + "]"
-    else:
-        info = "URL:" + os.path.basename(url)
-
-    sql = source_conn.cursor()
-
-    # let's see if we already know the file
-    __execute(sql, """
-        SELECT filestore_id
-        FROM filestore
-        WHERE unique_id=?
-    """, [file_unique_id])
-
-    for s_as in store_as:
-        if s_as in ["data", "url", "file"]:
-            if s_as == "file":
-                print(
-                    """Warning: store_as parameter "file" is currently unsupported. """)
-            continue
-        print("""Error: store_as parameter needs to be an Array of "data", "url", and/or "file".""")
-        sys.exit(1)
-
-    row = sql.fetchone()
-    if row:
-        filestore_id = row[0]
-        print("Notice: Re-used File: ", filestore_id, info)  # ,r.headers
-    else:
-        mimetype = None
-        blobdata = None
-        filesize = None
-        extra_info = []
-
-        if "data" in store_as:
-            if url.startswith("file://"):
-                fn = url[7:]  # cut of file
-                if not fn.startswith("/"):  # relative path, add cwd
-                    fn = os.getcwd() + "/" + fn
-                    # print("file name", fn)
-                    # print("file_unique_id", file_unique_id)
-
-                fl = open(fn, 'rb')
-
-                with fl:
-                    data = fl.read()
-
-                mimetype = None
-            else:
-                req = requests.get(url)
-                mimetype = req.headers["content-type"]
-                data = req.content
-
-            filesize = len(data)
-            blobdata = sqlite3.Binary(data)
-
-        if url.startswith("file://"):
-            filename = url[7:]
-        else:
-            filename = None
-
-        __execute(sql, """
-            INSERT INTO filestore
-            (unique_id, original_filename, filename, url, mimetype, filesize, data)
-            VALUES (?,?,?,?,?,?,?)
-        """, (
-            file_unique_id,
-            original_filename,
-            filename,
-            url,
-            mimetype,
-            filesize,
-            blobdata
-        ))
-
-        filestore_id = sql.lastrowid
-
-        if original_filename != None:
-            extra_info.append(original_filename)
-
-        if mimetype != None:
-            extra_info.append(str(mimetype))
-
-        if filesize != None:
-            extra_info.append(str(filesize) + "b")
-
-        if len(extra_info):
-            extra_info_txt = "(" + ",".join(extra_info) + ")"
-        else:
-            extra_info_txt = ""
-
-        if not silent:
-            print("Notice: File:", filestore_id, extra_info_txt.encode(
-                "utf8"), "stored in filestore.", info)
-
-    __execute(sql, """
-        INSERT INTO file
-        (filestore_id, source_name, source_table_name, source_unique_id, source_column_name, origin_url, eas_id, eas_root_id, file_version)
-        VALUES (?,?,?,?,?,?,?,?,?)
-    """, (
-        filestore_id,
-        name,
-        source_table_name,
-        source_unique_id,
-        source_column_name,
-        url,
-        eas_id,
-        eas_root_id,
-        file_version
-    ))
-
-    sql.close()
-
-    return
-
-
-def __human_size(size_bytes):
-    """
-    format a size in bytes into a 'human' file size, e.g. bytes, KB, MB, GB, TB, PB
-    Note that bytes/KB will be reported in whole numbers but MB and above will have greater precision
-    e.g. 1 byte, 43 bytes, 443 KB, 4.3 MB, 4.43 GB, etc
-    """
-    if size_bytes == 1:
-        return "1 byte"
-
-    suffixes_table = [('bytes', 0), ('KB', 0), ('MB', 1),
-                      ('GB', 2), ('TB', 2), ('PB', 2)]
-
-    num = float(size_bytes)
-    for suffix, precision in suffixes_table:
-        if num < 1024.0:
-            break
-        num /= 1024.0
-
-    if precision == 0:
-        formatted_size = "%d" % num
-    else:
-        formatted_size = str(round(num, ndigits=precision))
-
-    return "%s %s" % (formatted_size, suffix)
-
 # source is the output file of this script, the name source stems from the following step in the migration
-
-
 def prepare_source(
     # filename for the sqlite database file, where the results will be written into
     source="result.sqlite",
@@ -398,7 +113,7 @@ def prepare_source(
 
     try:
         if source_conn:
-            __commit_source()
+            commit_source()
     except NameError:
         pass
 
@@ -439,8 +154,7 @@ def prepare_source(
                 FROM filestore
             """).fetchone()
 
-            print("""Keeping filestore in source "%s", removing all origins. \nFilestore currently holds %s files in %s bytes.""" % (
-                source, row[0], __human_size(row[1])))
+            print("""Keeping filestore in source "%s", removing all origins. \nFilestore currently holds %s files in %s bytes.""" % (source, row[0], row[1]))
 
             # remove manually
             sql.execute("""
@@ -626,7 +340,6 @@ def remove_from_source(
     """, [name])
 
 
-##################################
 # K10+
 
 
