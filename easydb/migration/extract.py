5784cf343e023eb33a96ac91e38698698c124a8c
Philipp Hempel <philipp.hempel@programmfabrik.de>
2024-02-22 17:03:24 +0100

    updates of simple data2sqlite version which only parses k10plus data; see #65693

--- easydb/migration/extract.py
+++ easydb/migration/extract.py
@@ -100,7 +100,7 @@ def prepare_source(
     # filename for the sqlite database file, where the results will be written into
     source="result.sqlite",
     init=True,  # set True and source will be purged
-    init_filestore=None  # create crucial tables inside the target sqlite file
+    init_filestore=None,  # create crucial tables inside the target sqlite file
 ):
 
     global source_conn
@@ -131,41 +131,52 @@ def prepare_source(
     # FIXME: if Init==False: check for file existence!
 
     try:
-        source_conn = sqlite3.connect(
-            filename, detect_types=sqlite3.PARSE_DECLTYPES)
+        source_conn = sqlite3.connect(filename, detect_types=sqlite3.PARSE_DECLTYPES)
     except sqlite3.OperationalError as e:
         print("Error: Unable to open sqlite file: " + filename)
         raise e
 
-    source_conn.execute("""
+    source_conn.execute(
+        """
         PRAGMA foreign_keys=ON
-    """)
+    """
+    )
 
-    version = source_conn.execute("""
+    version = source_conn.execute(
+        """
         SELECT sqlite_version()
-    """).fetchone()[0]
+    """
+    ).fetchone()[0]
     print("""Sqlite %s, Version: %s connected.""" % (filename, version))
 
     if init:
         if not init_filestore:
             sql = source_conn.cursor()
-            row = sql.execute("""
+            row = sql.execute(
+                """
                 SELECT count(filestore_id), sum(filesize)
                 FROM filestore
-            """).fetchone()
+            """
+            ).fetchone()
 
-            print("""Keeping filestore in source "%s", removing all origins. \nFilestore currently holds %s files in %s bytes.""" % (source, row[0], row[1]))
+            print(
+                """Keeping filestore in source "%s", removing all origins. \nFilestore currently holds %s files in %s bytes."""
+                % (source, row[0], row[1])
+            )
 
             # remove manually
-            sql.execute("""
+            sql.execute(
+                """
                 SELECT DISTINCT(source_name)
                 FROM origin
-            """)
+            """
+            )
             for row in sql.fetchall():
                 remove_from_source(row[0])
             sql.close()
         else:
-            source_conn.execute("""
+            source_conn.execute(
+                """
                 CREATE TABLE origin (
                     origin_id INTEGER PRIMARY KEY,
                     origin_type TEXT NOT NULL,
@@ -176,11 +187,13 @@ def prepare_source(
                     UNIQUE(origin_type, origin_database_name, origin_table_name),
                     UNIQUE(source_name, source_table_name)
                 )
-            """)
+            """
+            )
 
             # for XML
 
-            source_conn.execute("""
+            source_conn.execute(
+                """
                 CREATE TABLE IF NOT EXISTS xmldata (
                     node_id_path TEXT NOT NULL,
                     node_element_path TEXT NOT NULL,
@@ -191,33 +204,42 @@ def prepare_source(
                     node_attrs TEXT,
                     node_data TEXT
                 )
-            """)
+            """
+            )
 
-            __execute(source_conn, """
+            __execute(
+                source_conn,
+                """
                 CREATE INDEX xmldata_idx ON xmldata (node_id_path)
-            """)
+            """,
+            )
 
-            source_conn.execute("""
+            source_conn.execute(
+                """
                 CREATE TABLE IF NOT EXISTS "xmldata.transcribed" (
                     node_id_path TEXT NOT NULL,
                     node_element_path TEXT NOT NULL
                 )
-            """)
+            """
+            )
 
             # K10plus
 
             # table that stores values which belong to items
-            source_conn.execute("""
+            source_conn.execute(
+                """
                 CREATE TABLE IF NOT EXISTS k10plus_source (
                     filename TEXT NOT NULL,
                     offset INTEGER NOT NULL,
                     from_item_id INTEGER,
                     to_item_id INTEGER
                 )
-            """)
+            """
+            )
 
             # table that stores values which belong to items (pica feld and unterfeld in rows)
-            source_conn.execute("""
+            source_conn.execute(
+                """
                 CREATE TABLE IF NOT EXISTS k10plus_data (
                     id INTEGER PRIMARY KEY,
                     item_id INTEGER NOT NULL,
@@ -225,7 +247,8 @@ def prepare_source(
                     unterfeld TEXT NOT NULL,
                     wert TEXT NOT NULL
                 )
-            """)
+            """
+            )
 
         print("Notice: Create Base Tables Sqlite Database \"%s\"" % filename)
     else:
@@ -234,20 +257,32 @@ def prepare_source(
     if init_filestore:
         if not init:
             print("Notice: Dropping Filestore Tables", filename)
-            source_conn.execute("""
+            source_conn.execute(
+                """
                 PRAGMA foreign_keys=OFF
-            """)
-            __execute(source_conn, """
+            """
+            )
+            __execute(
+                source_conn,
+                """
                 DROP TABLE IF EXISTS "file"
-            """)
-            __execute(source_conn, """
+            """,
+            )
+            __execute(
+                source_conn,
+                """
                 DROP TABLE IF EXISTS "filestore"
-            """)
-            source_conn.execute("""
+            """,
+            )
+            source_conn.execute(
+                """
                 PRAGMA foreign_keys=ON
-            """)
+            """
+            )
 
-        __execute(source_conn, """
+        __execute(
+            source_conn,
+            """
             CREATE TABLE file (
                 file_id INTEGER PRIMARY KEY,
                 filestore_id INTEGER NOT NULL,
@@ -265,18 +300,24 @@ def prepare_source(
                 UNIQUE(eas_id, file_version),
                 FOREIGN KEY (filestore_id) REFERENCES filestore(filestore_id) ON DELETE RESTRICT
             )
-        """)
+        """,
+        )
 
-        __execute(source_conn, """
+        __execute(
+            source_conn,
+            """
             CREATE INDEX file_idx ON file (
                 source_name,
                 source_table_name,
                 source_column_name,
                 source_unique_id
             )
-        """)
+        """,
+        )
 
-        __execute(source_conn, """
+        __execute(
+            source_conn,
+            """
             CREATE TABLE filestore (
                 filestore_id INTEGER PRIMARY KEY,
                 unique_id TEXT NOT NULL UNIQUE,
@@ -287,9 +328,11 @@ def prepare_source(
                 filename TEXT,
                 data BLOB
             )
-        """)
+        """,
+        )
 
         print("Notice: Created Filestore Tables:", filename)
+
 
 # remove all data from the source for a
 # specific name
@@ -297,7 +340,7 @@ def prepare_source(
 
 def remove_from_source(
     name,  # name of source
-    keep_filestore=False  # whether to keep or drop files from filestore
+    keep_filestore=False,  # whether to keep or drop files from filestore
 ):
     global source_conn
 
@@ -308,36 +351,55 @@ def remove_from_source(
         print("Removing matching files from filestore...")
 
     sql = source_conn.cursor()
-    __execute(sql, """
+    __execute(
+        sql,
+        """
         SELECT source_name, source_table_name
         FROM origin
         WHERE source_name=?
     """,
-              [name])
+        [name],
+    )
     for row in sql.fetchall():
         tntpl = (row[0], row[1])
-        __execute(sql, """
+        __execute(
+            sql,
+            """
             DROP TABLE IF EXISTS "%s.%s"
-        """ % tntpl)
+        """
+            % tntpl,
+        )
 
     if not keep_filestore:
-        __execute(sql, """
+        __execute(
+            sql,
+            """
             DELETE FROM filestore
             WHERE filestore_id IN (
                 SELECT filestore_id
                 FROM file
                 WHERE source_name=?
             )
-        """, [name])
-        __execute(sql, """
+        """,
+            [name],
+        )
+        __execute(
+            sql,
+            """
             DELETE FROM file
             WHERE source_name=?
-        """, [name])
+        """,
+            [name],
+        )
 
-    __execute(sql, """
+    __execute(
+        sql,
+        """
         DELETE FROM origin
         WHERE source_name=?
-    """, [name])
+    """,
+        [name],
+    )
 
 
 # K10+
@@ -350,11 +412,51 @@ DELIMITER_FALLBACK = '$'
 NEWLINE = '\n'
 
 KNOWN_SUB_FIELDS = [
-    '0', '2', '3', '4', '5', '6', '7', '8', '9',
-    'a', 'A', 'b', 'B', 'c', 'C', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'L', 'm', 'n',
-    'N', 'o', 'p', 'P', 'q', 'r', 'S', 's', 't',
-    'T', 'U', 'u', 'v', 'w', 'X', 'x', 'y', 'z'
+    '0',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'a',
+    'A',
+    'b',
+    'B',
+    'c',
+    'C',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'L',
+    'm',
+    'n',
+    'N',
+    'o',
+    'p',
+    'P',
+    'q',
+    'r',
+    'S',
+    's',
+    't',
+    'T',
+    'U',
+    'u',
+    'v',
+    'w',
+    'X',
+    'x',
+    'y',
+    'z',
 ]
 
 
@@ -422,7 +524,9 @@ def k10plus_get_offset(filename):
     global source_conn
     cur = source_conn.cursor()
 
-    __execute(cur, """
+    __execute(
+        cur,
+        """
         SELECT max(to_item_id)
         FROM k10plus_source
 
@@ -432,8 +536,9 @@ def k10plus_get_offset(filename):
         FROM k10plus_source
         WHERE filename = '{source}'
     """.format(
-        source=os.path.basename(filename)
-    ))
+            source=os.path.basename(filename)
+        ),
+    )
     rows = cur.fetchall()
     cur.close()
 
@@ -454,9 +559,9 @@ def k10plus_get_offset(filename):
 
 
 def k10plus_to_source(
-    filename,        # .pp filename
+    filename,  # .pp filename
     item_id_offset,  # last assigned item_id to start of when assigning new ids for new items
-    offset           # offset in the current file if items where already saved from this file
+    offset,  # offset in the current file if items where already saved from this file
 ):
     global source_conn
     cur = source_conn.cursor()
@@ -478,15 +583,15 @@ def k10plus_to_source(
     # if the file was not read yet (partially), add it to the source table
     if offset == 0:
         # insert a new entry in the source table
-        __execute(cur, """
+        __execute(
+            cur,
+            """
             INSERT INTO k10plus_source
             (filename, offset, from_item_id)
             VALUES (?,?,?)
-        """, (
-            basename,
-            0,
-            item_id_offset + 1
-        ))
+        """,
+            (basename, 0, item_id_offset + 1),
+        )
 
     try:
 
@@ -533,16 +638,15 @@ def k10plus_to_source(
                 for unterfeld in group:
                     for wert in group[unterfeld]:
                         # insert a new entry in the data table, add an uplink to the item
-                        __execute(cur, """
+                        __execute(
+                            cur,
+                            """
                             INSERT INTO k10plus_data
                             (item_id, feld, unterfeld, wert)
                             VALUES (?,?,?,?)
-                        """, (
-                            item_id,
-                            feld,
-                            unterfeld,
-                            wert
-                        ))
+                        """,
+                            (item_id, feld, unterfeld, wert),
+                        )
                         total_rows += 1
 
     except KeyboardInterrupt:
@@ -554,15 +658,15 @@ def k10plus_to_source(
         raise e
 
     # update offset and last item id in source table
-    __execute(cur, """
+    __execute(
+        cur,
+        """
         UPDATE k10plus_source
         SET offset = ?, to_item_id = ?
         WHERE filename = ?
-    """, (
-        cur_item - 1,
-        item_id,
-        basename
-    ))
+    """,
+        (cur_item - 1, item_id, basename),
+    )
 
     cur.close()
     if close_connection:
@@ -570,8 +674,16 @@ def k10plus_to_source(
 
     if item_id > 1000:
         print()
-    print(filename, '| duration:', (datetime.datetime.now() -
-          start), '|', total_items, 'items,', total_rows, 'rows')
+    print(
+        filename,
+        '| duration:',
+        (datetime.datetime.now() - start),
+        '|',
+        total_items,
+        'items,',
+        total_rows,
+        'rows',
+    )
 
     if interrupt:
         exit(0)
